f7e463e6a53fbc11fe00b8c4be53c82f9b43d80c
Dolmatova Varvara <2002varya@gmail.ru>
2023-06-29 23:10:37 +0300

    fix: duplicate comments

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let updateTimer = undefined;
 
-function LoadNews() {
+function loadNews() {
     $("#content").html("");
     $("#content").load("./components/newsPosts.html");
     $.ajax({
@@ -11,8 +11,7 @@ function LoadNews() {
             data = data.sort((a, b) => b.time - a.time);
             $.get('./components/card.html', (cardText) => {
                 data.forEach(post => {
-                    console.log(post.id);
-                    card = jQuery(cardText);
+                    card = $(cardText);
                     card.find('#title').text(post.title);
                     card.find('#rating').text('Points: ' + post.points);
                     card.find('#date').text('Was posted: ' + post.time_ago);
@@ -24,13 +23,13 @@ function LoadNews() {
 
         },
         error: (error) => {
-            console.log(error);
+
         }
     })
     return true;
 }
 
-function LoadOneNews() {
+function loadOneNews() {
     const news_id = window.location.hash.replace('#', '');
     $("#content").html("");
     const post = $("#content").load("./components/newsPost.html");
@@ -40,8 +39,6 @@ function LoadOneNews() {
         cache: false,
         dataType: 'json',
         success: (data) => {
-
-            console.log(data);
             post.find('#title').text(data.title);
             post.find('#rating').text('Points: ' + data.points);
             post.find('#date').text('Was posted: ' + data.time_ago);
@@ -52,19 +49,18 @@ function LoadOneNews() {
             $.get('./components/comment.html', (cardText) => {
                 const doc = new DOMParser();
                 data.comments.forEach(el => {
-                    post.find('#comments').first().append(CreateCommentCard(cardText, doc, el, false));
+                    post.find('#comments').first().append(createCommentCard(cardText, doc, el, false));
                 })
             });
         },
         error: (error) => {
-            console.log(error);
+            
         }
     })
     return true;
 }
 
-function CreateCommentCard(cardText, doc, element, recursive) {
-    console.log('try to add comments')
+function createCommentCard(cardText, doc, element, recursive) {
     const card = jQuery(cardText);
     card.find('#user').text(element.user);
     card.find('#time_comm').text(element.time_ago);
@@ -76,16 +72,17 @@ function CreateCommentCard(cardText, doc, element, recursive) {
     else{
         card.find('#show_button').on('click', () => { 
             element.comments.forEach(el => {
-                card.find('#comments').first().append(CreateCommentCard(cardText, doc, el, true));
+                card.find('#comments').first().append(createCommentCard(cardText, doc, el, true));
             })
+            card.find('#show_button').attr('hidden', true);
         })
     }
     if (recursive){
         element.comments.forEach(el => {
-            card.find('#comments').first().append(CreateCommentCard(cardText, doc, el, true));
+            card.find('#comments').first().append(createCommentCard(cardText, doc, el, true));
         })
     }
-    return card
+    return card;
 }
 
 
@@ -94,16 +91,16 @@ function changePage() {
     const hash = window.location.hash.replace('#', '');
     clearInterval(updateTimer);
     if (hash === "") {
-        LoadNews();
-        $('#refresh_button').on('click', LoadNews);
-        $('#refresh_button').off('click', LoadOneNews);
-        updateTimer = setInterval(LoadNews, 60000);
+        loadNews();
+        $('#refresh_button').on('click', loadNews);
+        $('#refresh_button').off('click', loadOneNews);
+        updateTimer = setInterval(loadNews, 60000);
     }
     else if (parseInt(hash)) {
-        LoadOneNews(hash);
-        $('#refresh_button').on('click', LoadOneNews);
-        $('#refresh_button').off('click', LoadNews);
-        updateTimer = setInterval(LoadOneNews, 60000);
+        loadOneNews(hash);
+        $('#refresh_button').on('click', loadOneNews);
+        $('#refresh_button').off('click', loadNews);
+        updateTimer = setInterval(loadOneNews, 60000);
     }
     return true;
 }
